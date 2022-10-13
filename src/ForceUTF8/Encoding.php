6e7304afe85b85ea72ee0289438ff0e75426458b
Aurélien Fontaine <afontaine@campings.com>
2022-10-13 09:45:28 +0200

    Fix php8.1 compatibility (add missing cast)

--- src/ForceUTF8/Encoding.php
+++ src/ForceUTF8/Encoding.php
@@ -203,7 +203,7 @@ class Encoding {
                     $buf .= $c1 . $c2;
                     $i++;
                 } else { //not valid UTF8.  Convert it.
-                    $cc1 = (chr((int) (ord($c1) / 64)) | "\xc0");
+                    $cc1 = self::firstByte($c1);
                     $cc2 = ($c1 & "\x3f") | "\x80";
                     $buf .= $cc1 . $cc2;
                 }
@@ -212,7 +212,7 @@ class Encoding {
                     $buf .= $c1 . $c2 . $c3;
                     $i = $i + 2;
                 } else { //not valid UTF8.  Convert it.
-                    $cc1 = (chr((int) (ord($c1) / 64)) | "\xc0");
+                    $cc1 = self::firstByte($c1);
                     $cc2 = ($c1 & "\x3f") | "\x80";
                     $buf .= $cc1 . $cc2;
                 }
@@ -221,12 +221,12 @@ class Encoding {
                     $buf .= $c1 . $c2 . $c3 . $c4;
                     $i = $i + 3;
                 } else { //not valid UTF8.  Convert it.
-                    $cc1 = (chr(ord($c1) / 64) | "\xc0");
+                    $cc1 = self::firstByte($c1);
                     $cc2 = ($c1 & "\x3f") | "\x80";
                     $buf .= $cc1 . $cc2;
                 }
             } else { //doesn't look like UTF8, but should be converted
-                    $cc1 = (chr(ord($c1) / 64) | "\xc0");
+                    $cc1 = self::firstByte($c1);
                     $cc2 = (($c1 & "\x3f") | "\x80");
                     $buf .= $cc1 . $cc2;
             }
@@ -234,7 +234,7 @@ class Encoding {
               if(isset(self::$win1252ToUtf8[ord($c1)])) { //found in Windows-1252 special cases
                   $buf .= self::$win1252ToUtf8[ord($c1)];
               } else {
-                $cc1 = (chr(ord($c1) / 64) | "\xc0");
+                $cc1 = self::firstByte($c1);
                 $cc2 = (($c1 & "\x3f") | "\x80");
                 $buf .= $cc1 . $cc2;
               }
@@ -348,4 +348,13 @@ class Encoding {
     }
     return $o;
   }
+
+    /**
+     * @param $c1
+     * @return string
+     */
+    static function firstByte($c1)
+    {
+        return (chr((int)(ord($c1) / 64)) | "\xc0");
+    }
 }
